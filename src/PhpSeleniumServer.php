253ec0cd86fabb210151ea390143221a8562f0f6
hayatravis <hayato.sgwr@gmail.com>
2017-11-17 13:48:26 +0900

    fix command

--- src/PhpSeleniumServer.php
+++ src/PhpSeleniumServer.php
@@ -44,7 +44,7 @@ class PhpSeleniumServer
     public function startSeleniumServer()
     {
         if (!$this->isStopSeleniumServer()) throw new PhpSeleniumServerException('It already started.');
-        exec('nohup ' . $this->seleniumServerPath . ' > /dev/null & echo $!', $output);
+        exec('nohup ' . $this->seleniumServerPath . ' > /dev/null & echo $! 2>&1', $output);
         $this->pid = $output[0];
     }
 
